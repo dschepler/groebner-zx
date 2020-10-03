334f9b9fd08b9bf4d63fe5557200505a5a6ee355
Daniel Schepler <dschepler@gmail.com>
2020-10-03 12:27:23 -0700

    In interactive mode, add an abbreviated version of the header suggested by GPLv3 text

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 
 int main() {
   if (isatty(STDIN_FILENO)) {
+    std::cout << "groebner-zx  Copyright (C) 2020  Daniel Schepler\n";
+    std::cout << "This program comes with ABSOLUTELY NO WARRANTY.\n";
+    std::cout << "This is free software, and you are welcome to redistribute it\n";
+    std::cout << "under the conditions of the GNU General Public License v3\n\n";
     std::cout << "Enter the list of polynomials, one on each line as a list of coefficients\n";
     std::cout << "e.g. enter x^5 - 3x^2 + x as: 1 0 0 -3 1 0\n";
     std::cout << "Then end the list with a blank line or EOF\n";
